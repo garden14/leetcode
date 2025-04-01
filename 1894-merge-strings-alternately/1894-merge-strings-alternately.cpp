a11071f7cefe596eb71899d0f2a0cda0ce76d2a4
garden14 <113659080+garden14@users.noreply.github.com>
2025-04-01 20:48:42 +0900

    Time: 0 ms (100%), Space: 8.5 MB (31.01%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        
+        int n = word1.length();
+        int m = word2.length();
+
+        string result = "";
+
+        int i = 0;
+
+        while(i < n || i < m) {
+            if(i < n) {
+                result += word1[i];
+            }
+            if(i < m) {
+                result += word2[i];
+            }
+            i++;
+        }
+
+        return result;
     }
 };
